9351568bd5a26c25a5e3e6c935ae4433a854ea2f
Fredrik Johansen <fredrik1206@gmail.com>
2025-12-05 21:31:09 +0100

    feat(2025-02): adds solution for part two for 2025 day 02

--- src/2025/02/index.ts
+++ src/2025/02/index.ts
@@ -27,3 +27,31 @@ export const part1 = (input: string[]): number => {
 
   return sequenceNumbers.reduce((prev, curr) => prev + curr, 0);
 };
+
+export const part2 = (input: string[]): number => {
+  const sequenceNumbers: Set<number> = new Set();
+
+  for (const sequence of input) {
+    const [startNumber, endNumber] = sequence.split("-");
+
+    const parsedStartNumber = parseInt(startNumber);
+    const parsedEndNumber = parseInt(endNumber);
+
+    for (let index = parsedStartNumber; index <= parsedEndNumber; index++) {
+      const numberString = index.toString();
+      const numberStringLength = numberString.length;
+
+      for (let part = 1; part < numberStringLength; part++) {
+        const regex = new RegExp(String.raw`[\s\S]{1,${part}}`, "g");
+        const characterParts = numberString.match(regex) ?? [];
+        const characterPartsSet: Set<string> = new Set(characterParts);
+
+        if (characterPartsSet.size === 1) {
+          sequenceNumbers.add(parseInt(numberString));
+        }
+      }
+    }
+  }
+
+  return [...sequenceNumbers].reduce((prev, curr) => prev + curr, 0);
+};
